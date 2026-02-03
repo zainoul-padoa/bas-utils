a1db5716653c5fb5c93b4bcdf31528958686040b
zainoul-padoa <zainoul.h.ext@padoa-group.com>
2026-02-03 16:17:42 +0100

    add Formula Status (Victor) in Accounts and Deals

--- zoho/import_zoho_tables.py
+++ zoho/import_zoho_tables.py
@@ -17,4 +17,40 @@ if __name__ == "__main__":
 			print(f"Inserting {table_name} into database...")
 			df.to_sql(table_name, con=connection, schema=SCHEMA_NAME, index=False)
 			print(f"✓ Table {table_name} inserted")
+
+	# Add "Formula Status (Victor)" to ACCOUNTS
+	print(f"Add columln Formula Status (Victor) to Accounts...")
+		# create column
+	connection.execute('''alter table zoho."Accounts" 
+						add "Formula Status (Victor)" text''')
+		# no active Deal by default
+	connection.execute('''update zoho."Accounts" a 
+						set "Formula Status (Victor)" = ''No Active Deal''
+					''')
+		# active deal if there is a Deal around today
+	connection.execute('''update zoho."Accounts" 
+						set "Formula Status (Victor)" = ''Active Deal''
+						where "Id" in (
+						select a."Id"
+						from  zoho."Accounts" a left join zoho."Deals" d  on d."Account_Name" = a."Id" 
+						where cast(now() as timestamp) between cast(d."Datum_Vertragsbeginn" as timestamp) and cast(d."Datum_Vertragsende" as timestamp) ) 
+					''')
+	print("✓ Column Formula Status (Victor) inserted in Accounts")
+
+	# Add "Account Status (Formula Victor)" to DEALS
+		# create column
+	connection.execute('''alter table zoho."Deals" 
+							add "Account Status (Formula Victor)" text''')
+		# no active deal by default
+	connection.execute('''update zoho."Deals" 
+							set "Account Status (Formula Victor)"  = ''No Active Deal''
+						''')
+		# active deal is the company has an active deal (cf. Accounts)
+	connection.execute('''update zoho."Deals" 
+						set "Account Status (Formula Victor)" = ''Active Deal''
+						where "Account_Name" in (
+						select a."Id"  from zoho."Accounts" a where "Formula Status (Victor)" = ''Active Deal''
+						)
+					''')
+
 	connection.close()
