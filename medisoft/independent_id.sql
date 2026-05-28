060a3606c5895f94a93bf44d4d14ed473323d3f2
Zainoul Hedaraly <zainoul.h.ext@padoa-group.com>
2026-05-28 14:47:19 +0200

    independent id remove rostock filter

--- medisoft/independent_id.sql
+++ medisoft/independent_id.sql
@@ -11,5 +11,4 @@ SELECT setval('bas_firms.medisoft_independent', 130007950);
 INSERT INTO bas_firms.easybill_medisoft (medisoft_id, id)
 SELECT b.rec_id , nextval('bas_firms.medisoft_independent') FROM table_beschaeftigte b 
 LEFT JOIN table_firmenstruktur f ON f.rec_id = b.ebetrieb_id 
-WHERE lower(f.kuerzel) like '%selbstzahler%'
-AND lower(f.mandant) = 'rostock' ;
+WHERE lower(f.kuerzel) like '%selbstzahler%';
