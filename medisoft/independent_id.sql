29788413258ee92ab09e405ba24338df6ee38f4a
zainoul-padoa <zainoul.h.ext@padoa-group.com>
2026-05-28 14:41:19 +0200

    generate independent firm id

---
+++ medisoft/independent_id.sql
@@ -0,0 +1,15 @@
+---------- INDEPENDENT ID ------------
+-- get max id
+SELECT * FROM bas_firms.easybill_medisoft em
+ORDER BY id DESC;
+
+-- set sequence to that id
+CREATE SEQUENCE bas_firms.medisoft_independent;
+SELECT setval('bas_firms.medisoft_independent', 130007950); 
+
+-- add id to easybill_medisoft
+INSERT INTO bas_firms.easybill_medisoft (medisoft_id, id)
+SELECT b.rec_id , nextval('bas_firms.medisoft_independent') FROM table_beschaeftigte b 
+LEFT JOIN table_firmenstruktur f ON f.rec_id = b.ebetrieb_id 
+WHERE lower(f.kuerzel) like '%selbstzahler%'
+AND lower(f.mandant) = 'rostock' ;
