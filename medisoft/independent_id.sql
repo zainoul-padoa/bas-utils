272766878d4cbab17e2b9778cde04f89d783da97
Zainoul Hedaraly <zainoul.h.ext@padoa-group.com>
2026-06-04 20:07:20 +0200

    add schema before table name

--- medisoft/independent_id.sql
+++ medisoft/independent_id.sql
@@ -9,8 +9,8 @@ SELECT setval('bas_firms.medisoft_independent', 130007950);
 
 -- add id to easybill_medisoft
 INSERT INTO bas_firms.easybill_medisoft (medisoft_id, id)
-SELECT b.rec_id , nextval('bas_firms.medisoft_independent') FROM table_beschaeftigte b 
-LEFT JOIN table_firmenstruktur f ON f.rec_id = b.ebetrieb_id 
+SELECT b.rec_id , nextval('bas_firms.medisoft_independent') FROM medisoft.table_beschaeftigte b 
+LEFT JOIN medisoft.table_firmenstruktur f ON f.rec_id = b.ebetrieb_id 
 WHERE lower(f.kuerzel) like '%selbstzahler%';
 
 -- employee without company
@@ -24,5 +24,5 @@ SELECT setval('bas_firms.medisoft_independent', 130016749);
 
 -- add id to easybill_medisoft
 INSERT INTO bas_firms.easybill_medisoft (medisoft_id, id)
-SELECT b.rec_id , nextval('bas_firms.medisoft_independent') FROM table_beschaeftigte b 
+SELECT b.rec_id , nextval('bas_firms.medisoft_independent') FROM medisoft.table_beschaeftigte b 
 WHERE inactive AND ebetrieb_id IS NULL;
