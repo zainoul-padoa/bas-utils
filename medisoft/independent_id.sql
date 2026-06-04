d83da40a1cdf5dc383f69953a78cf8bef554ef2e
Zainoul Hedaraly <zainoul.h.ext@padoa-group.com>
2026-06-04 20:13:24 +0200

    Add check to avoid duplicate medisoft_id inserts
    Prevent duplicate entries in easybill_medisoft by checking existing medisoft_id.

--- medisoft/independent_id.sql
+++ medisoft/independent_id.sql
@@ -26,4 +26,5 @@ SELECT setval('bas_firms.medisoft_independent', 130016749);
 -- add id to easybill_medisoft
 INSERT INTO bas_firms.easybill_medisoft (medisoft_id, id)
 SELECT b.rec_id , nextval('bas_firms.medisoft_independent') FROM medisoft.table_beschaeftigte b 
-WHERE inactive AND ebetrieb_id IS NULL;
+WHERE inactive AND ebetrieb_id IS NULL
+AND b.rec_id NOT IN (SELECT medisoft_id FROM bas_firms.easybill_medisoft) -- if it is already in easybill_medisoft, don't insert it again please!
