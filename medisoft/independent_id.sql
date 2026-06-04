6d16b07c40205b2bbfb493a9c79f64a7adae52d6
Zainoul Hedaraly <zainoul.h.ext@padoa-group.com>
2026-06-04 20:11:53 +0200

    Prevent duplicate inserts in easybill_medisoft
    Added a condition to prevent duplicate entries in easybill_medisoft.

--- medisoft/independent_id.sql
+++ medisoft/independent_id.sql
@@ -11,7 +11,8 @@ SELECT setval('bas_firms.medisoft_independent', 130007950);
 INSERT INTO bas_firms.easybill_medisoft (medisoft_id, id)
 SELECT b.rec_id , nextval('bas_firms.medisoft_independent') FROM medisoft.table_beschaeftigte b 
 LEFT JOIN medisoft.table_firmenstruktur f ON f.rec_id = b.ebetrieb_id 
-WHERE lower(f.kuerzel) like '%selbstzahler%';
+WHERE lower(f.kuerzel) like '%selbstzahler%'
+AND b.rec_id NOT IN (SELECT medisoft_id FROM bas_firms.easybill_medisoft) -- if it is already in easybill_medisoft, don't insert it again please!
 
 -- employee without company
 ---- GENERATE INDEPENT FIRM SOURCE ID
