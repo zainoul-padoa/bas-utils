18cff9c7c2a5e71973ed2765059d4f7efa8dbf09
Zainoul Hedaraly <zainoul.h.ext@padoa-group.com>
2026-06-04 17:01:50 +0200

    employee without company

--- medisoft/independent_id.sql
+++ medisoft/independent_id.sql
@@ -12,3 +12,17 @@ INSERT INTO bas_firms.easybill_medisoft (medisoft_id, id)
 SELECT b.rec_id , nextval('bas_firms.medisoft_independent') FROM table_beschaeftigte b 
 LEFT JOIN table_firmenstruktur f ON f.rec_id = b.ebetrieb_id 
 WHERE lower(f.kuerzel) like '%selbstzahler%';
+
+-- employee without company
+---- GENERATE INDEPENT FIRM SOURCE ID
+-- get max id
+SELECT * FROM bas_firms.easybill_medisoft em
+ORDER BY id DESC;
+
+-- set sequence to that id
+SELECT setval('bas_firms.medisoft_independent', 130016749); 
+
+-- add id to easybill_medisoft
+INSERT INTO bas_firms.easybill_medisoft (medisoft_id, id)
+SELECT b.rec_id , nextval('bas_firms.medisoft_independent') FROM table_beschaeftigte b 
+WHERE inactive AND ebetrieb_id IS NULL;
