32a8861cb83cfc165566996ce3da37cd36b9c341
Zainoul Hedaraly <zainoul.h.ext@padoa-group.com>
2026-06-05 11:04:56 +0200

    Fix SQL condition to exclude null medisoft_ids

--- medisoft/independent_id.sql
+++ medisoft/independent_id.sql
@@ -12,7 +12,7 @@ INSERT INTO bas_firms.easybill_medisoft (medisoft_id, id)
 SELECT b.rec_id , nextval('bas_firms.medisoft_independent') FROM medisoft.table_beschaeftigte b 
 LEFT JOIN medisoft.table_firmenstruktur f ON f.rec_id = b.ebetrieb_id 
 WHERE lower(f.kuerzel) like '%selbstzahler%'
-AND b.rec_id NOT IN (SELECT medisoft_id FROM bas_firms.easybill_medisoft) -- if it is already in easybill_medisoft, don't insert it again please!
+AND b.rec_id NOT IN (SELECT medisoft_id FROM bas_firms.easybill_medisoft and medisoft_id is not null) -- if it is already in easybill_medisoft, don't insert it again please!
 
 -- employee without company
 ---- GENERATE INDEPENT FIRM SOURCE ID
