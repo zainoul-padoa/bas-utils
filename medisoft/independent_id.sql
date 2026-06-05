4f29ed0661825acee7626d5d14c6bc760acf18b0
Zainoul Hedaraly <zainoul.h.ext@padoa-group.com>
2026-06-05 11:07:20 +0200

    Update SQL insert condition for easybill_medisoft

--- medisoft/independent_id.sql
+++ medisoft/independent_id.sql
@@ -27,4 +27,4 @@ SELECT setval('bas_firms.medisoft_independent', 130016749);
 INSERT INTO bas_firms.easybill_medisoft (medisoft_id, id)
 SELECT b.rec_id , nextval('bas_firms.medisoft_independent') FROM medisoft.table_beschaeftigte b 
 WHERE inactive AND ebetrieb_id IS NULL
-AND b.rec_id NOT IN (SELECT medisoft_id FROM bas_firms.easybill_medisoft) -- if it is already in easybill_medisoft, don't insert it again please!
+AND b.rec_id NOT IN (SELECT medisoft_id FROM bas_firms.easybill_medisoft where medisoft_id is not null) -- if it is already in easybill_medisoft, don't insert it again please!
