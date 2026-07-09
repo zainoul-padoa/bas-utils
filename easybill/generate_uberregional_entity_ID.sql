c2fc54d195b75b1bcefab5d7be68f9279d3d2c72
Zainoul Hedaraly <zainoul.h.ext@padoa-group.com>
2026-07-09 11:25:23 +0200

    Rename padoa_id to old_padoa_id in ue_id table

--- easybill/generate_uberregional_entity_ID.sql
+++ easybill/generate_uberregional_entity_ID.sql
@@ -16,6 +16,9 @@ SET id = nextval('bas_firms.easybill_merge_seq')
 ALTER TABLE bas_firms.uberregional_entity  
 ADD COLUMN padoa_final_id text;
 
+ALTER TABLE ue_id 
+RENAME COLUMN padoa_id TO old_padoa_id;
+
 -- before any modification, backup
 CREATE TABLE bas_firms.uberregional_entity_backup_20260706_1328 AS 
 SELECT * FROM bas_firms.uberregional_entity;
@@ -23,7 +26,7 @@ SELECT * FROM bas_firms.uberregional_entity;
 
 -- give generated ID to the couple mother_id - padoa_id
 UPDATE bas_firms.uberregional_entity ue
-SET padoa_final_id = (SELECT uei.id FROM ue_id uei WHERE uei.mother_id = ue.mother_id AND uei.padoa_id = ue.padoa_id)
+SET padoa_final_id = (SELECT uei.id FROM ue_id uei WHERE uei.mother_id = ue.mother_id AND uei.old_padoa_id = ue.padoa_id)
 ; 
 
 -- oops we generated an ID when mother_id = padoa_id too, we shouldn't have
